38c7bb15333517d221508e45ad8b1d5ea5955b03
Christian <salasch2@gmail.com>
2026-06-20 12:12:07 -0700

    lint fixes

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,5 @@
 // utils/supabase.ts
 import { createClient } from '@supabase/supabase-js';
-import type { Database } from '../types/database.types';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
