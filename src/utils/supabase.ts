ac90134d3e455ce405b4532b076be1320e04e1d1
Christian <salasch2@gmail.com>
2026-08-26 02:42:42 -0700

    revert back

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,8 +1,10 @@
 // utils/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+
+console.log(supabaseUrl)
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   db: { schema: 'public' },
